10215f0cfe76f615c8b1b4e75ffd0e3cd75a076f
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2023-12-05 22:34:41 +0700

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -14,21 +14,23 @@ soup_page = BeautifulSoup(xml_page, "xml")
 news_items = soup_page.find_all("item")
 
 # Define the keyword you want to search for
-search_keyword = "Gibran"  # Replace with your specific keyword
+# search_keyword = ""  # Replace with your specific keyword
 
 # Extract news items that contain the keyword
 filtered_news = []
 for item in news_items:
     title = item.title.text if item.title else "N/A"
     # Check if the keyword is in the title
-    if search_keyword.lower() in title.lower():
-        # Extracting the news link and publication date (optional)
-        link = item.link.text if item.link else "N/A"
-        pubDate = item.pubDate.text if item.pubDate else "N/A"
-        filtered_news.append(
-            {"Title": title, "Link": link, "Published Date": pubDate})
+    # if search_keyword.lower() in title.lower():
+    # Extracting the news link and publication date (optional)
+    link = item.link.text if item.link else "N/A"
+    pubDate = item.pubDate.text if item.pubDate else "N/A"
+    filtered_news.append(
+        {"Title": title, "Link": link, "Published Date": pubDate})
 
 # Display the first few news items that match the keyword
-for news in filtered_news[:5]:
+for news in filtered_news:
     print(
         f"Title: {news['Title']}")
+    print(f"Link: {news['Link']}")
+    print("\n")
